e4ed5b164077e3d0fc5c5637f97b3f4f13c3a006
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-10-27 11:41:08 +0200

    add proper license

--- physycom/jsontohtml.hpp
+++ physycom/jsontohtml.hpp
@@ -1,21 +1,7 @@
 /* Copyright 2015-2017 - Alessandro Fabbri, Stefano Sinigardi */
 
-/***************************************************************************
-This file is part of utils.
-
-utils is free software : you can redistribute it and / or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-utils is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with utils. If not, see <http://www.gnu.org/licenses/>.
-***************************************************************************/
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt
 
 
 #ifndef UTILS_JSONTOHTML_HPP
