97c174b47a627e707da222a343feb08d8db75639
Alessandro Fabbri <rommacc@yahoo.it>
2017-11-22 17:50:26 +0000

    add cdf and nico's python like interface to histo

--- physycom/histo.hpp
+++ physycom/histo.hpp
@@ -11,6 +11,7 @@
 #include <vector>
 #include <map>
 #include <fstream>
+#include <numeric>
 
 namespace physycom
 {
@@ -22,30 +23,37 @@ namespace physycom
     T min, max, binw;
     int nmin = 100000, nmax = 0;
     int nbin;
+    std::string xlabel, ylabel, title;
 
     histo() {};
 
-    histo(T min_, T max_, T binw_)
+    histo(T min_, T max_, T binw_, std::string xl = "Xlabel", std::string yl = "Counter", std::string t = "Title")
     {
       min = min_;
       max = max_;
       binw = binw_;
       nbin = int((max - min) / binw);
+      xlabel = xl;
+      ylabel = yl;
+      title = t;
     }
 
     // this won't compile with int data
     // but it's handy for floating point
-    histo(T min_, T max_, int nbin_)
+    histo(T min_, T max_, int nbin_, std::string xl = "Xlabel", std::string yl = "Counter", std::string t = "Title")
     {
       min = min_;
       max = max_;
       nbin = nbin_;
       binw = (max - min) / nbin;
+      xlabel = xl;
+      ylabel = yl;
+      title = t;
     }
 
     void count(const std::string &tag, const T &t)
     {
-      if( counter[tag].size() == 0 ) counter[tag].resize(nbin, 0);
+      if( counter[tag].size() != nbin ) counter[tag].resize(nbin, 0);
       int idx = int((t - min) / binw);
       if( idx < 0 ) return;
       counter[tag][(idx > nbin - 1) ? nbin - 1 : idx]++;
@@ -67,9 +75,17 @@ namespace physycom
 
     void dump(const std::string &filename)
     {
+      // evaluate totals for normalization
+      std::map<std::string, int> tot;
+      std::map<std::string, double> cumulate;
+      std::for_each(counter.begin(), counter.end(), [&tot](const std::pair<std::string, std::vector<int>> &p)
+      {
+        tot[p.first] = std::accumulate(p.second.begin(), p.second.end(), 0);
+      });
+      
       std::ofstream outhisto(filename);
       outhisto << "  bin  ";
-      for (const auto &label : counter) outhisto << label.first << "  ";
+      for (const auto &label : counter) outhisto << label.first << "-cnt  " << label.first << "-cdf  ";
       outhisto << std::endl;
       for (int i = 0; i < nbin; ++i)
       {
@@ -79,7 +95,8 @@ namespace physycom
           int cnt = counter[label.first][i];
           nmin = ( nmin < cnt ) ? nmin : cnt;
           nmax = ( nmax > cnt ) ? nmax : cnt;
-          outhisto << cnt << "\t";
+
+          outhisto << cnt << "\t" << (cumulate[label.first] += cnt / (double) tot[label.first]) << "\t";
         }
         outhisto << std::endl;
       }
@@ -109,7 +126,7 @@ set style line 16 lc rgb '#4dbeee' linetype 2 linewidth linew # light - blue
 set style line 17 lc rgb '#a2142f' linetype 1 linewidth linew # red
 set style line 21 lc rgb '#a2142f' pointtype 7 linewidth 0.2 # red
 # Grid and Ticks
-set ytics 0, )" << int(1.2 * nmax/5) << ", " << 10*nmax << R"( nomirror out scale 0.75
+set ytics 0, )" << int(1.2 * nmax / 5) << ", " << 10 * nmax << R"( nomirror out scale 0.75
 set style line 102 lc rgb '#d6d7d9' lt 1 lw 1
 set grid xtics ytics back ls 102
 # More options
@@ -119,17 +136,70 @@ set style data histograms
 set key top center autotitle columnhead
 set xrange[-1:)" << nbin << R"(]
 set yrange[)" << nmin << ":" << 1.2 * nmax << R"(]
-set title 'Title'
-set xlabel 'Xlabel'
-set ylabel 'Counter'
+set title ')" << title << R"('
+set xlabel ')" << xlabel << R"('
+set ylabel ')" << ylabel << R"('
 set xtics border in scale 0, 0 nomirror rotate by - 45
 plot ')" << basename << R"(.txt')";
 
       int cnt = 0, column = 2, style = 11;
       for (const auto &p : counter)
       {
-        outplt << ((cnt == 0) ? "" : "     ''") << " using " << column++ << ":xtic(int($0) % 2 == 0 ? stringcolumn(1) : '') title '" << p.first << "' ls " << style++ << ((cnt != counter.size() - 1) ? " ,\\" : "") << std::endl;
+        outplt << ((cnt == 0) ? "" : "     ''") << " using " << column << ":xtic(int($0) % 2 == 0 ? stringcolumn(1) : '') title '" << p.first << "' ls " << style++ << ((cnt != counter.size() - 1) ? " ,\\" : "") << std::endl;
+        column += 2;
         ++cnt;
+      }
+      outplt.close();
+    }
+
+    void gnuplot_cdf(const std::string &filename) const
+    {
+      std::string basename = filename.substr(0, filename.find_first_of("."));
+      std::ofstream outplt(filename);
+      outplt << R"(set terminal pngcairo  transparent enhanced font "Verdana,20" fontscale 0.8 size 960, 720 background rgb 'white'
+set output ')" << basename << R"(.cdf.png'
+# Border
+set style line 101 lc rgb '#808080' lt 1 lw 1
+set border 11 front ls 101
+set tics nomirror out scale 0.75
+set format '%.0s %c'
+set border linewidth 1.5
+# Styles
+linew = 1.2
+ptsize = 1.5
+set style line 11 lc rgb '#0072bd' lt 1 lw linew # blue
+set style line 12 lc rgb '#d95319' lt 1 lw linew # orange
+set style line 13 lc rgb '#edb120' lt 1 lw linew # yellow
+set style line 14 lc rgb '#7e2f8e' lt 1 lw linew # purple
+set style line 15 lc rgb '#77ac30' lt 1 lw linew # green
+set style line 16 lc rgb '#4dbeee' lt 2 lw linew # light - blue
+set style line 17 lc rgb '#a2142f' lt 1 lw linew # red
+set style line 21 lc rgb '#d95319' pointtype 7 lw linew ps ptsize # orange
+# Grid and Ticks
+set ytics 0, )" << int(1.2 * nmax/5) << ", " << 10*nmax << R"( nomirror out scale 0.75
+set y2tics 0, 10, 110 nomirror out scale 0.35
+set style line 102 lc rgb '#d6d7d9' lt 1 lw 1
+set grid xtics ytics back ls 102
+# More options
+set style fill solid 1.00 border lt - 1
+set style histogram clustered gap 1 title textcolor lt - 1
+set style data histograms
+set key top left autotitle columnhead
+set xrange[-1:)" << nbin << R"(]
+set yrange[)" << nmin << ":" << 1.2 * nmax << R"(]
+set y2range[0:105]
+set title ')" << title << R"('
+set xlabel ')" << xlabel << R"('
+set ylabel ')" << ylabel << R"('
+set y2label 'Percentage'
+set xtics border in scale 0, 0 nomirror rotate by - 45
+plot ')" << basename << R"(.txt')";
+
+      int cnt = 0, column = 2, hstyle = 11, lpstyle = 21;
+      for (const auto &p : counter)
+      {
+        outplt << ((cnt == 0) ? "" : "     ''") << " using " << column++ << ":xtic(int($0) % 2 == 0 ? stringcolumn(1) : '') title '" << p.first << "' ls " << hstyle++ << ((cnt != 2 * counter.size() - 1) ? " ,\\" : "") << std::endl; cnt++;
+        outplt << ((cnt == 0) ? "" : "     ''") << " using (column(0)):($" << column++ << "*100) title '" << p.first << "-cdf' with linespoints ls " << lpstyle++ << " axes x1y2 " << ((cnt != 2 * counter.size() - 1) ? " ,\\" : "") << std::endl; cnt++;
       }
       outplt.close();
     }
@@ -140,13 +210,14 @@ plot ')" << basename << R"(.txt')";
   struct multihisto
   {
     std::map<std::string, histo<T>> hs;
-    void add_histo(std::string name, T min, T max, T binw) { hs[name] = histo<T>(min, max, binw); }
-    void add_histo(std::string name, T min, T max, int nbin) { hs[name] = histo<T>(min, max, nbin); }
+    void add_histo(std::string name, T min, T max, T binw, std::string xl = "Xlabel", std::string yl = "Counter", std::string t = "Title") { hs[name] = histo<T>(min, max, binw, xl, yl, t); }
+    void add_histo(std::string name, T min, T max, int nbin, std::string xl = "Xlabel", std::string yl = "Counter", std::string t = "Title") { hs[name] = histo<T>(min, max, nbin, xl, yl, t); }
     void count(std::string name, std::string tag, T t) { hs[name].count(tag, t); }
     void push(std::string name, std::string tag, T t) { hs[name].data[tag].push_back(t); }
     void populate() { for(auto &h : hs) h.second.populate(); }
     void dump() { for(auto &h : hs) h.second.dump("histo_" + h.first + ".txt"); }
-    void gnuplot() { for(auto &h : hs) h.second.gnuplot("histo_" + h.first + ".plt"); }
+    void gnuplot() { for (auto &h : hs) h.second.gnuplot("histo_" + h.first + ".plt"); }
+    void gnuplot_cdf() { for(auto &h : hs) h.second.gnuplot_cdf("histo_" + h.first + ".cdf.plt"); }
   };
 
   template<typename T>
