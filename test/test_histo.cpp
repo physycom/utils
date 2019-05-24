917bc350eefd3688366def79751dca1bbb0b19c9
allefabbri <rommacc@yahoo.it>
2019-05-24 17:47:00 +0200

    add discrete distribution to test_histo

--- test/test_histo.cpp
+++ test/test_histo.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <random>
+#include <algorithm>
 
 #include <physycom/histo.hpp>
 
@@ -39,11 +40,26 @@ int main()
   auto gammas = std::vector<std::gamma_distribution<double>>();
   for(auto p : gamma_p) gammas.push_back(std::gamma_distribution<double>(p.first, p.second));
 
+  // discrete exp
+  int nval = 20, start_val = 100;
+  std::vector<int> values(nval);
+  std::iota(values.begin(), values.end(), start_val);
+  double binw = 500.;
+  std::vector<double> distc({ 1500., 3100., 5000. });
+  std::vector<std::vector<double>> probs(distc.size());
+  std::vector<std::discrete_distribution<int>> dexps;
+  for(int n=0; n<(int)distc.size(); ++n)
+  {
+    for(int i=0; i<nval; ++i) probs[n].emplace_back(std::exp(-i*binw/distc[n])/distc[n]);
+    dexps.emplace_back(std::discrete_distribution<int>(probs[n].begin(), probs[n].end()));
+  }
+
   // histograms
   multihisto<double> mh;
   mh.add_histo("exp", 0., 20., 25, "x", "Counter", "Exponential Distribution");
   mh.add_histo("norm", -2., 3., 25, "x", "Counter", "Normal Distribution");
   mh.add_histo("gamma", 0., 20., 25, "x", "Counter", "Gamma Distribution");
+  mh.add_histo("dexp", start_val - 1, start_val + nval + 1, nval + 2, "x", "Counter", "Discrete Exp Distribution");
   int Nrolls = 10000;
   for(int i=0; i<Nrolls; ++i)
   {
@@ -61,6 +77,11 @@ int main()
     {
       auto x = gammas[n](mt);
       mh.count("gamma","k=" + std::to_string(gamma_p[n].first) + " th=" + std::to_string(gamma_p[n].second), x);
+    }
+    for(int n=0; n < dexps.size(); ++n)
+    {
+      auto x = dexps[n](mt);
+      mh.count("dexp","dc = " + std::to_string(distc[n]), double(values[x]));
     }
   }
   mh.dump("histo/");
