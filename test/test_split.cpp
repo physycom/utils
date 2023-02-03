5733182f0a40bdc08825f116cbac4f857a3ec211
Stefano Sinigardi <stesinigardi@hotmail.com>
2023-02-03 16:24:53 +0100

    remove catch.hpp and add a vcpkg dependency to it

--- test/test_split.cpp
+++ test/test_split.cpp
@@ -3,12 +3,10 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt
 
-
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
-
 #include <boost/algorithm/string.hpp>
 #include <chrono>
+#include <catch.hpp>
+
 #include "physycom/string.hpp"
 
 
